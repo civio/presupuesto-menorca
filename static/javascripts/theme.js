5ba56f314b5554f8491e9042df8a768597147cbf
David Cabo <david.cabo@gmail.com>
2024-08-12 18:06:41 +0200

    Add execution data for 2024Q1 and revert budget to 2023 (extended) civio/presupuesto-management#1334

--- static/javascripts/theme.js
+++ static/javascripts/theme.js
@@ -1,5 +1,19 @@
 // Theme custom js methods
 $(document).ready(function(){
 
-  // Add here your custom code
+  var addYearSelectorCustomLabels = function(){
+    var str2024 = {
+      'es': 'prorrogado',
+      'ca': 'prorrogat',
+    };
+
+    $('.data-controllers .layout-slider .slider .slider-tick-label').each(function(){
+      var val = $(this).html();
+      if (val === '2024'){
+        $(this).html(val + '<br/><small><i> ('+ str2024[ $('html').attr('lang') ] +')</i></small>');
+      }
+    });
+  };
+
+  addYearSelectorCustomLabels();
 });
